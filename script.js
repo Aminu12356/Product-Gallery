32faa1935fb5515939132157436bf7d9c0ff58a8
aminu audu <aminuaudu61@gmail.com>
2026-06-24 11:34:52 +0100

    Implement product gallery with animations and theme switcher

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const toggleButton = document.getElementById("theme-toggle");
 toggleButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 
+  
   if (document.body.classList.contains("dark-mode")) {
     toggleButton.textContent = "☀️ Light Mode";
   } else {
